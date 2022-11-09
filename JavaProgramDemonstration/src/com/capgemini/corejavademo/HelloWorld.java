406ecb83d3c55f55261b46c2145a25c59b746570
win10 <win10@DESKTOP-1AJQ02F>
2022-11-09 12:08:10 +0530

    First Commit

---
+++ JavaProgramDemonstration/src/com/capgemini/corejavademo/HelloWorld.java
@@ -0,0 +1,18 @@
+package array;
+
+public class test {
+	
+	public static void main(string[] args) {
+		int arr[]=new int[5];
+		
+		arr[0]=4;
+		arr[1]=6;
+		arr[2]=9;
+		arr[3]=2;
+		arr[4]=8;
+		arr[5]=9;
+		for (int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
